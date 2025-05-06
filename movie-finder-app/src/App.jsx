82718026adbc80c141b8638398474dae52022982
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-05-06 18:49:48 +0530

    Basic Functions Done

--- movie-finder-app/src/App.jsx
+++ movie-finder-app/src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css';
+import './css/App.css';
 import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
+import Favourites from './pages/Favourites';
+import { MovieProvider } from './contexts/MovieContext';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favourites" element={<Favourites />}/>
+        </Routes>
+      </main>
+    </MovieProvider>
   )
 }
 
